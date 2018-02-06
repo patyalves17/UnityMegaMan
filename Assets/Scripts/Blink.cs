6cacc162b202c9b4c16bcbce1d041d35aa6a5f69
logonrm <logonrm@fiap.com.br>
2018-02-05 22:46:27 -0200

    tela inicial

---
+++ Assets/Scripts/Blink.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+
+public class Blink : MonoBehaviour {
+
+	//Intervalo para ligar/desligar a renderizacao
+	public float intervalo;
+
+	IEnumerator Start () {
+
+		//obtem o camponente do objeto
+		GetComponent<SpriteRenderer> ().enabled = false;
+		yield return new WaitForSeconds (intervalo);
+		GetComponent<SpriteRenderer> ().enabled = true;
+		yield return new WaitForSeconds (intervalo);
+
+		//faz a chamada novamente do metodo deixado em looping
+		StartCoroutine(Start());
+	}
+
+	void update (){
+		//pressionar enter
+		if(Input.GetKeyDown(KeyCode.Return)){
+			SceneManager.LoadScene ("game-scene");
+		}
+	}
+
+}
